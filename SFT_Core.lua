6167a7e60f967c535cf83fdd1b234d056f9f9591
Sam Gray <samleegray@gmail.com>
2026-02-23 15:22:38 -0800

    Refines Fillet Tracker UI and logic
    Reduces the height of the fillet tracker window and adjusts label offsets for improved visual appearance. Removes Perfect Roe inventory label and its update logic to streamline UI. Adds dynamic resizing of the fillet window to fit the stats label content, enhancing adaptability.
    
    These changes aim to optimize the roe tracking interface by simplifying the display, focusing on essential stats, and improving layout flexibility.
    
    Branch: roe_tracking

--- SFT_Core.lua
+++ SFT_Core.lua
@@ -11,27 +11,27 @@ SFT.total_bag = 0
 SFT.total_bank = 0
 
 SFT.constants = {
-    roeRate = 0.008,
-    averageRateWindowSeconds = 300,
-    windowHeightFull = 105,
-    windowHeightCollapsed = 80,
-    filletWindowHeight = 55,
-    filletWindowWidth = 300,
-    icons = {
-        fish = "esoui/art/icons/crafting_fishing_perch.dds",
-        bag = "esoui/art/tooltips/icon_bag.dds",
-        bank = "esoui/art/tooltips/icon_bank.dds",
-        roe = "/esoui/art/icons/crafting_heavy_armor_vendor_component_002.dds",
-    },
-    perfectRoeItemId = 64222,
-    excludedFishItemIds = {
-        [100393] = true,
-        [100394] = true,
-        [100395] = true,
-    },
-    visibility = {
-        HIDE = 0,
-        SHOW = 1,
-        AUTO = 2,
-    },
+  roeRate = 0.008,
+  averageRateWindowSeconds = 300,
+  windowHeightFull = 105,
+  windowHeightCollapsed = 80,
+  filletWindowHeight = 35,
+  filletWindowWidth = 300,
+  icons = {
+    fish = "esoui/art/icons/crafting_fishing_perch.dds",
+    bag = "esoui/art/tooltips/icon_bag.dds",
+    bank = "esoui/art/tooltips/icon_bank.dds",
+    roe = "/esoui/art/icons/crafting_heavy_armor_vendor_component_002.dds",
+  },
+  perfectRoeItemId = 64222,
+  excludedFishItemIds = {
+    [100393] = true,
+    [100394] = true,
+    [100395] = true,
+  },
+  visibility = {
+    HIDE = 0,
+    SHOW = 1,
+    AUTO = 2,
+  },
 }
